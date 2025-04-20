88c3072208a940b63329d1e03cb5a78900757a1e
Ojasvi2004 <ojasvisaini2111@gmail.com>
2025-04-20 15:17:22 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import Latest from '../components/latest'
+import Latest from '../components/Latest'
 import Bestseller from '../components/Bestseller'
 import Fill from '../components/Fill'
 import Explore from '../components/Explore'
